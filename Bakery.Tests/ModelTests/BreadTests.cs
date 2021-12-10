2031321b7aaa7f6f4bf354c698aa2702e711f3a0
Ella Tanttu <ellajtanttu@gmail.com>
2021-12-10 09:03:50 -0800

    add boilerplate code to files

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,9 @@
+using System; // directive - tells it what code it needs in order to function.
+using System.Collections.Generic; // if using list objects
+
+namespace Bakery.Models
+{
+  public class Pastry//class
+  {
+  }
+}
