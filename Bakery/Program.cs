d492250896064551e9b7526c1f8b2be0226da4ea
Ella Tanttu <ellajtanttu@gmail.com>
2021-12-10 13:49:32 -0800

    catches errors from incorrect input format

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -16,31 +16,56 @@ class Program//class
     Console.WriteLine("Pastry - $2.00 (bulk discounts available)");
     Console.WriteLine("---------------------------");
     Console.WriteLine("How many loaves of bread would you like to purchase?");
-    int userBreadNum = int.Parse(Console.ReadLine());
-    int breadTotal = userBread.BreadCost(userBreadNum);
-    Console.WriteLine("How many pastries would you like to purchase?");
-    int userPastryNum = int.Parse(Console.ReadLine());
-    int pastryTotal = userPastry.PastryCost(userPastryNum);
-    Console.WriteLine(" ");
-    Console.WriteLine("Thank you! Your Receipt:");
-    int orderTotal = breadTotal + pastryTotal;
-    Console.WriteLine($"Bread Total: ${breadTotal}.00");
-    Console.WriteLine($"Pastry Total: ${pastryTotal}.00");
-    Console.WriteLine($"TOTAL: ${orderTotal}.00");
-    Console.WriteLine("---------------------------");
-    Console.WriteLine("Would you like to place another order? ['Y' for yes, 'Enter' for no]");
-    string newOrderAnswer = Console.ReadLine();
-    if (newOrderAnswer == "Y" || newOrderAnswer == "y")
+    try
     {
-      Main();
+      int userBreadNum = int.Parse(Console.ReadLine());
+      int breadTotal = userBread.BreadCost(userBreadNum);
+      Console.WriteLine("How many pastries would you like to purchase?");
+      int userPastryNum = int.Parse(Console.ReadLine());
+      int pastryTotal = userPastry.PastryCost(userPastryNum);
+      Console.WriteLine(" ");
+      Console.WriteLine("Thank you! Your Receipt:");
+      int orderTotal = breadTotal + pastryTotal;
+      Console.WriteLine($"Bread Total: ${breadTotal}.00");
+      Console.WriteLine($"Pastry Total: ${pastryTotal}.00");
+      Console.WriteLine($"TOTAL: ${orderTotal}.00");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("Would you like to place another order? ['Y' for yes, 'Enter' for no]");
+      string newOrderAnswer = Console.ReadLine();
+      if (newOrderAnswer == "Y" || newOrderAnswer == "y")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Thanks for your business. Have a wonderful day!");
+      }
     }
-    else
+    catch
     {
-      Console.WriteLine("Thanks for your business. Have a wonderful day!");
+      Console.WriteLine("Please enter a whole number only.");
+      Main();
     }
 
   }
 }
+
+// try
+// {
+//   Console.WriteLine($"Your word is worth {newWord.GetScore()} points");
+//   Console.WriteLine("Current list of entered words with values");
+//   foreach (ScrabbleLetter currentWord in resultList)
+//   {
+//     Console.WriteLine($"{currentWord.Word} - {newWord.GetScore()}");
+//   }
+//   Console.WriteLine($"Total Score is: {ScrabbleLetter.GetTotalScore()}");
+// }
+// catch (Exception)
+// {
+//   Console.WriteLine("Word must only be letters");
+// }
+
+
 
 // string addItemAnswer = Console.ReadLine();
 //     if (addItemAnswer == "Y" || addItemAnswer == "y" )
