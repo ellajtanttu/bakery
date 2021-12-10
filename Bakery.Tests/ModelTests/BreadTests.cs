bb295db3a69a903341d66908c7299ba2dad638fe
Ella Tanttu <ellajtanttu@gmail.com>
2021-12-10 09:46:19 -0800

    add plain english specs and brainstorming

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -6,3 +6,69 @@ using System;
 namespace Bakery.Tests
 {
 }
+
+/*
+Create a C# console application for a bakery that includes the
+following functionality:
+
+X - There should be two classes: one for Bread and one for Pastry.
+
+- When the user runs the application, they should receive a prompt
+with a welcome message along with the cost for both Bread and Pastry.
+
+- A user should be able to specify how many loaves of Bread and how
+many Pastrys they'd like.
+
+The application will return the total cost of the order.
+
+Pierre offers the following deals:
+
+Bread:
+- Buy 2, get 1 free (every 3rd loaf of bread is free.)
+- A single loaf costs $5, two loaves costs $10, and three loaves cost $10.
+
+Pastry:
+- Buy 1 for $2 or 3 for $5.
+- Four pastries costs $7, five pastries costs $9, and six pastries costs $10.
+
+All functionality for the models should be tested.
+
+
+Order of events:
+1. prompt with a welcome message along with the cost for both Bread and
+Pastry, and question of how much of each
+2. User enters amounts of each type
+3. TO FIGURE OUT: logic separation for the total cost
+  - BreadCost():
+    - logic for the cost of bread
+  - pastryCost():
+    - logic for the cost of pastry
+  - both will take a number as a parameter
+
+4. return message with final cost to user, (and details of discount)
+
+Specs Plain English:
+Bread:
+Example input: BreadCost(1)
+Example output: 5
+
+Example input: BreadCost(2)
+Example output: 10
+
+Example input: BreadCost(3)
+Example output: 10, Deal! - add "Deal!" only for mults of 3
+
+Example input: BreadCost(4)
+Example output: 15,
+
+Example input: BreadCost(6)
+Example output: 20
+
+Example input: BreadCost(20)
+Example output: 70
+
+Example input: BreadCost(36)
+Example output: 120
+
+
+*/
--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -6,3 +6,24 @@ using System;
 namespace Bakery.Tests
 {
 }
+
+/*
+Pastry:
+Example input: PastryCost(1)
+Example output: 2
+
+Example input: PastryCost(2)
+Example output: 4
+
+Example input: PastryCost(3)
+Example output: 5
+
+Example input: PastryCost(4)
+Example output: 7
+
+Example input: PastryCost(5)
+Example output: 9
+
+Example input: PastryCost(6)
+Example output: 10
+*/
